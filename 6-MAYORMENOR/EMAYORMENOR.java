3aa79bcbf194781e490291f15cce93322f63cf86
zavalacristina <zavalacristina199@gmail.com>
2023-05-25 09:27:38 -0600

    se agrega archivo EMAYORMENOR

---
+++ 6-MAYORMENOR/EMAYORMENOR.java
@@ -0,0 +1,17 @@
+
+package MAYORYMENOR;
+
+
+public class EMAYORMENOR {
+    public String retornamultiplo(int numero){
+        if(numero <=17){
+           return "Eres menor de edad"; 
+        }else if(numero>=18){
+            return "Eres mayor de edad";
+        }
+        return "NINGUNO";
+}
+    
+}
+
+
