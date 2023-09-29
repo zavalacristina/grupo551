3a6abe17c5de55b072dc6fc852dd6af9da15189e
zavalacristina <zavalacristina199@gmail.com>
2023-09-29 11:56:37 -0600

    se persona

---
+++ unidad1/persona.java
@@ -0,0 +1,61 @@
+
+package temaarrays;
+
+
+public class persona {
+    private int num;
+    private String nombre;
+    private int edad;
+    
+    public persona(){
+        
+    }
+    public persona(int num, String nombre, int edad){
+        this.num=num;
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+
+    /**
+     * @return the num
+     */
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * @param num the num to set
+     */
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
